f8af514ea23d2f6fc28f18693cdc7f595878930e
on malka <onmalka.tlp@gmail.com>
2021-05-28 09:18:52 +0300

    Added headless workaround with screenshot

--- whatsapp message sender.js.js
+++ whatsapp message sender.js.js
@@ -2,10 +2,14 @@ const puppeteer = require("puppeteer");
 
 async function sendWhatsappMassage(contactName, message) {
   try {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36");
     await page.goto("https://web.whatsapp.com/");
+
+    await page.waitForSelector('#app > div._3h3LX._1dqoA.app-wrapper-web.font-fix > div > div.landing-window > div.landing-main > div > div.O1rXL > div > canvas');
+    const element = await page.$('#app > div._3h3LX._1dqoA.app-wrapper-web.font-fix > div > div.landing-window > div.landing-main');
+    await element.screenshot({path: 'C:/Users/eshus/Desktop/whatsapp message sender qr code.png'});
 
     await page.waitForSelector("#pane-side > div:nth-child(1) > div > div > div:nth-child(11) > div > div > div.TbtXF > div._2pkLM > div._3Dr46 > span");
     
@@ -41,9 +45,11 @@ async function sendWhatsappMassage(contactName, message) {
       console.log('evaluated');
     await page.keyboard.press('Enter');
     console.log('Enter pressed');
+    await page.waitForTimeout(500);
+    await browser.close(); 
   } catch (e) {
     console.error("error mine", e);
   }
 };
 
-sendWhatsappMassage("Test", "good enough!!!");
+sendWhatsappMassage("Test", "headless is working!!!");
